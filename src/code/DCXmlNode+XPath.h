9291284fe80e659ba18b7da9fcf2dad42bbb0a97
drekka <d4rkf1br3@gmail.com>
2010-01-23 00:08:51 +1100

    updating readme

--- src/code/DCXmlNode+XPath.h
+++ src/code/DCXmlNode+XPath.h
@@ -13,9 +13,10 @@
  * This category adds xpath capability to the DCXmlNode. The Xpath capability is not a complete implementation. However it proves the basic set of instructions needed to be able to locate DCDMNodes. 
  * 
  * Here is a list of the elements you can use in the xpaths and how they are processed.
- * - <b>//<i>element-name</i></b> - returns all occurances of <i>element-name</i>, no matter where they occur in the document model below the current part in the path.
- * - <b>/</b> - delimiter between nodes of the xpath. If this occurs at the start of the xpath then it causes the path to be regarded as an absolute path which starts from the root of the document.
- * - <b>..</b> - refers back up to the parent of the current element. ie. When given to the Body element, the xpath "../Header" will return the header node.
+ * 
+ * - <b>//<i>element-name</i></b> - returns all occurances of <i>element-name</i> from the current point in the document model and below.
+ * - <b>/</b> - delimiter between nodes of the xpath. <b>Note:</b> If this occurs at the start of the xpath then it causes the path to be regarded as an absolute path which starts from the root of the document.
+ * - <b>..</b> - refers back up to the parent of the current element. 
  * - <b>.</b> - refers to the current node so doesn't really do anything as such, but is here for completeness. Allows xpaths like "./abc"
  * - <b>element-name</b> - looks in the list of sub elements of the current element, and returns the first which has the specified element-name.
  * - <b>[nn]</b> - Finds a sub node of the current node base on a zero based index. ie. [2] returns the second element under the current element. This is most useful when dealing with a list of items being returned.
